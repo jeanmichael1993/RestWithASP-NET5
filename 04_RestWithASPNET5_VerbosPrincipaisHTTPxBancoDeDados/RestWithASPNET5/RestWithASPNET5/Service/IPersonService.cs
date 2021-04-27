aa30fcb00352e7ecd990c12f8b7bde4f1ae6b7b0
jeanmichael1993 <jeanmichael760@gmail.com>
2021-04-26 22:43:38 -0300

    Projeto 04 verbors http usando banco Mysql

---
+++ 04_RestWithASPNET5_VerbosPrincipaisHTTPxBancoDeDados/RestWithASPNET5/RestWithASPNET5/Service/IPersonService.cs
@@ -0,0 +1,20 @@
+﻿using RestWithASPNET5.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5.Service
+{
+    //metodos implementados
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+    }
+}
